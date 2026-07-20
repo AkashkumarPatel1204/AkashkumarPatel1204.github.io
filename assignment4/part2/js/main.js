349c9914500c09cccd655b4c2efd5237613e0197
AkashkumarPatel1204 <Akashkumarpankajbhai.patel@dcmail.ca>
2026-07-20 10:41:32 -0400

    updating functions for main.js

--- assignment4/part2/js/main.js
+++ assignment4/part2/js/main.js
@@ -1,5 +1,39 @@
+//requirenment: constant array of image file names
+const imageFiles = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+
+// select required elements
 const displayedImage = document.querySelector(".displayed-img");
 const thumbBar = document.querySelector(".thumb-bar");
-
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+ 
+ // requirement: loop starting at 1
+ for (let i=1; i <= imageFiles.length; 
+		i++)
+		{ 
+		// create thumbnail <img>
+		const newImage = document.createElement("img");
+		newImage.src = `https://mdn.github.io/shared-assets/images/examples/learn/gallery/pic${i}.jpg`;
+		newImage.alt= `Thumbnail ${i}`;
+		
+		// add thumbnail to page
+		thumbBar.appendChild(newImage);
+
+		//requirenment: add click event listner to each thumbnail
+		newImage.addEventListener("click", () => {
+			displayedImage.src = newImage.src;
+			displayedImage.alt = newImage.alt;});
+}
+// requirenment: lighten/darken button logic
+btn.addEventListener("click", () => {
+	const btnClass = btn.getAttribute("class");
+	if (btnClass === "dark") {
+		// lighten imagebtn.setAttribute("class", "light");
+		btn.textContent = "Lighten";
+		overlay.style.backgroundColor = "rgba(0,0,0,0)";}
+		else {
+		// darken image
+		btn.setAttribute("class", "dark");
+		btn.textContent = "Darken";
+		overlay.style.backgroundColor = "rgba(0,0,0,0.5)";}
+});
