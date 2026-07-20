c9be873aecb5ae51c591e2029d5c9e4ba9f364c9
AkashkumarPatel1204 <Akashkumarpankajbhai.patel@dcmail.ca>
2026-07-20 12:03:04 -0400

    changing css to original

--- assignment4/part2/js/main.js
+++ assignment4/part2/js/main.js
@@ -30,11 +30,11 @@ btn.addEventListener("click", () => {
 	if (btnClass === "dark") {
     btn.setAttribute("class", "light");
     btn.textContent = "Lighten";
-    overlay.style.backgroundColor = "rgba(0,0,0,0)";
+    overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
 	} else {
     btn.setAttribute("class", "dark");
     btn.textContent = "Darken";
-    overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+    overlay.style.backgroundColor = "rgba(0,0,0,0)";
 	}
 
 });
